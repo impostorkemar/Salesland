21b236ab3d41781cac937e94274f8f0e7319b11d
Jefferson Cajas <38546533+impostorkemar@users.noreply.github.com>
2023-02-13 08:49:04 -0500

    changes

--- Python_codes/API/FASTAPI-MYSQL-RESTAPI-mabeSys/config/db.py
+++ Python_codes/API/FASTAPI-MYSQL-RESTAPI-mabeSys/config/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://mabeSys:rootmabe@localhost:3306/mabesys")
+#engine = create_engine("mysql+pymysql://mabeSys:rootmabe@localhost:3306/mabesys")
+engine = create_engine("mysql+pymysql://dmhost11_mabeSys:root_mabe@localhost:3306/dmhost11_mabesys")
 meta = MetaData()
 
 conn = engine.connect()
