8bf364318df9086fe7324279d1e6c1d25d55711c
Jefferson Cajas <38546533+impostorkemar@users.noreply.github.com>
2023-02-23 17:03:33 -0500

    chnges

--- Test/mabeSys/src/app/components/vacaciones/agregar-vacacion/agregar-vacacion.component.ts
+++ Test/mabeSys/src/app/components/vacaciones/agregar-vacacion/agregar-vacacion.component.ts
@@ -2,21 +2,16 @@ import { Component,OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, FormsModule } from '@angular/forms';
 import { CrudService } from 'src/app/services/crud.service';
 import { Router } from '@angular/router';
-import { NgbDate, NgbCalendar, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { JsonPipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-agregar-vacacion',
-	imports: [NgbDatepickerModule, FormsModule, JsonPipe],
   templateUrl: './agregar-vacacion.component.html',
   styleUrls: ['./agregar-vacacion.component.css'],
 })
 export class AgregarVacacionComponent {
-  hoveredDate: NgbDate | null = null;
 
-	fromDate: NgbDate;
-	toDate: NgbDate | null = null;
 
   formularioDeVacacion:FormGroup;
 
@@ -24,15 +19,12 @@ export class AgregarVacacionComponent {
     public formulario:FormBuilder,
     private crudService:CrudService,
     private ruteador:Router,
-    private calendar: NgbCalendar
   ) { 
     this.formularioDeVacacion = this.formulario.group({      
       vaca_disp:[''],
       dias_tomados:[''],
       
     });
-    this.fromDate = calendar.getToday();
-		this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
   }
 
   ngOnInit(): void {
@@ -49,34 +41,5 @@ export class AgregarVacacionComponent {
       
   }
 
-  onDateSelection(date: NgbDate) {
-		if (!this.fromDate && !this.toDate) {
-			this.fromDate = date;
-		} else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
-			this.toDate = date;
-		} else {
-			this.toDate = null;
-			this.fromDate = date;
-		}
-	}
-
-	isHovered(date: NgbDate) {
-		return (
-			this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
-		);
-	}
-
-	isInside(date: NgbDate) {
-		return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
-	}
-
-	isRange(date: NgbDate) {
-		return (
-			date.equals(this.fromDate) ||
-			(this.toDate && date.equals(this.toDate)) ||
-			this.isInside(date) ||
-			this.isHovered(date)
-		);
-	}
-
+  
 }
