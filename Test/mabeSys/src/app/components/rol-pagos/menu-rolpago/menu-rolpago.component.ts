8f1dfc45154b331c42e404544ef4f424aba354f2
Jefferson Cajas <38546533+impostorkemar@users.noreply.github.com>
2023-04-20 10:38:04 -0500

    Revert "Revert "Cambios Edwin""
    This reverts commit ba4c751fdf5d5dd51e40b5810d51190e6c0741c5.

--- Test/mabeSys/src/app/components/rol-pagos/menu-rolpago/menu-rolpago.component.ts
+++ Test/mabeSys/src/app/components/rol-pagos/menu-rolpago/menu-rolpago.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder,FormGroup } from '@angular/forms';
+import { CrudService } from 'src/app/services/crud.service';
 
 
 @Component({
@@ -8,5 +9,37 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./menu-rolpago.component.css']
 })
 export class MenuRolpagoComponent {
+  cedula: any;
+  nombre: any;
+  apellido: any;
+  cargo: any;
+  inicioContrato: any;
+  user!:String;
+  passw!:String;
+  formularioDeVacacion:FormGroup;
+
+  constructor(
+    private crudService:CrudService,
+    public formulario:FormBuilder,
+  ) {
+    this.user = localStorage.getItem('USER') as string;
+    this.passw = localStorage.getItem('PASS') as string
+    this.formularioDeVacacion = this.formulario.group({      
+      motivo:[''],      
+    });
+   }
+
+   ngOnInit(): void {
+    this.crudService.ObtenerDatosRolPagos().subscribe(respuesta => {
+      console.log(respuesta);
+      this.nombre = respuesta.nombre;
+      this.cedula = respuesta.cedula;
+      this.apellido = respuesta.apellido;
+      this.inicioContrato = respuesta.fecha_inicio_contrato;
+      this.cargo = respuesta.nombre_cargo
+       })
+   }
+
+
 
 }
