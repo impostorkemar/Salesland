f51dee2a567f80ed2a844a23dfb108ddc03a1186
EdwinIza <eizac@est.ups.edu.ec>
2023-05-22 13:13:54 -0500

    Cambios estilos Viajes

--- Test/mabeSys/src/app/components/viajes/menu-viajes/menu-viajes.component.ts
+++ Test/mabeSys/src/app/components/viajes/menu-viajes/menu-viajes.component.ts
@@ -14,73 +14,72 @@ export class MenuViajesComponent {
   control_vistas: FormGroup;
   rolVacations!: Boolean;
   rolPersonal!: Boolean;
-  rolReportes!: Boolean; 
+  rolReportes!: Boolean;
   probarRol_IV!: Boolean;
   probarRol_RU!: Boolean;
-  probarRol_D!: Boolean;  
+  probarRol_D!: Boolean;
   probarRol_IPV!: Boolean;
-  opcion:number =1;
-  nombreVentana:string = "";
+  opcion: number = 1;
+  nombreVentana: string = "";
 
   constructor(
     private fb: FormBuilder,
     private testuserService: TestuserService,
     private crudService: CrudService,
-    private router:Router,
-  ) { 
-    
-    this.control_vistas = this.fb.group({
-      name_usuario: ['',Validators.required],
-      lastname_usuario: ['',Validators.required],
-      cedula: ['',Validators.required],
-      cuenta: ['',Validators.required],
-      stateVacaciones: ['',Validators.required],
-      statePersonal: ['',Validators.required],
-      stateReportes: ['',Validators.required],   
+    private router: Router,
+  ) {
 
+    this.control_vistas = this.fb.group({
+      name_usuario: ['', Validators.required],
+      lastname_usuario: ['', Validators.required],
+      cedula: ['', Validators.required],
+      cuenta: ['', Validators.required],
+      stateVacaciones: ['', Validators.required],
+      statePersonal: ['', Validators.required],
+      stateReportes: ['', Validators.required],
     });
     this.opcion = 1;
   }
 
   ngOnInit(): void {
-    this.nombreVentana="";    
-    let Array: string[]=[];
-    let Array2: string[]=[];
-    this.mostrarDatos();    
-    this.control_vistas.controls['name_usuario'].disable();    
-    this.control_vistas.controls['lastname_usuario'].disable();    
-    this.control_vistas.controls['cedula'].disable();    
-    this.control_vistas.controls['cuenta'].disable();    
+    this.nombreVentana = "";
+    let Array: string[] = [];
+    let Array2: string[] = [];
+    this.mostrarDatos();
+    this.control_vistas.controls['name_usuario'].disable();
+    this.control_vistas.controls['lastname_usuario'].disable();
+    this.control_vistas.controls['cedula'].disable();
+    this.control_vistas.controls['cuenta'].disable();
     this.opcion = 0;
     this.mostrarDataUser();
+    this.isActive(1);
   }
 
-
-  mostrarDatos(){
+  mostrarDatos() {
     //console.log("MENU ROL:",localStorage.getItem('ROLE') as string)
-    if (localStorage.getItem('ROLE') as string == 'ROLE_ADMIN'){
+    if (localStorage.getItem('ROLE') as string == 'ROLE_ADMIN') {
       //console.log("PROBAR: admin");             
       this.rolVacations = true;
-      this.rolPersonal = true; 
-      this.rolReportes = true;  
-    }if (localStorage.getItem('ROLE') as string ==='ROLE_SUPERVISOR') {
+      this.rolPersonal = true;
+      this.rolReportes = true;
+    } if (localStorage.getItem('ROLE') as string === 'ROLE_SUPERVISOR') {
       //console.log("PROBAR: supervisor"); 
       this.rolVacations = true;
-      this.rolPersonal = true; 
-      this.rolReportes = false;   
-    }if (localStorage.getItem('ROLE') as string ==='ROLE_USER') {
+      this.rolPersonal = true;
+      this.rolReportes = false;
+    } if (localStorage.getItem('ROLE') as string === 'ROLE_USER') {
       //console.log("PROBAR: promotor");
       this.rolVacations = true;
-      this.rolPersonal = false; 
-      this.rolReportes = false;   
+      this.rolPersonal = false;
+      this.rolReportes = false;
     }
     this.menuViajes2(1)
   }
 
-  mostrarDataUser(){
+  mostrarDataUser() {
     var user = localStorage.getItem('USER') as string;
     var passw = localStorage.getItem('PASS') as string
-    this.crudService.ObtenerDataPersonaByUserAndPass(user,passw).subscribe(respuesta=>{
+    this.crudService.ObtenerDataPersonaByUserAndPass(user, passw).subscribe(respuesta => {
       //console.log("DATAUSER:",respuesta);
       this.control_vistas.setValue({
         name_usuario: respuesta['nombre'],
@@ -93,23 +92,27 @@ export class MenuViajesComponent {
       });
     });
   }
-  
-  menuViajes2(op: number){
-    console.log("value:",op)
-    if (op as unknown as string == '1'){      
-      this.nombreVentana='INGRESO-VIAJES';
+
+  menuViajes2(op: number) {
+    console.log("value:", op)
+    this.opcion = op;
+    if (op === 1) {
+      this.nombreVentana = 'INGRESO-VIAJES';
       //console.log("OPCION:",this.nombreVentana);
-      
-    }else if (op as unknown as string == '2'){      
-      this.nombreVentana='REPORTE-PERSONAL';
+
+    } else if (op === 2) {
+      this.nombreVentana = 'REPORTE-PERSONAL';
       //console.log("OPCION:",this.nombreVentana);
-    }else{
+    } else {
       //console.log("OPCION: VACIO");
     }
-  } 
+  }
 
-  goToMenuBotones(){
+  goToMenuBotones() {
     this.router.navigate(['/inicio']);
   }
 
+  isActive(op: number): boolean {
+    return this.opcion === op;
+  }
 }
