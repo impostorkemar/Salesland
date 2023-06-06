d8f89cfc68a0b2f1a463ac17a3a8c8f7c42e329b
EdwinIza <eizac@est.ups.edu.ec>
2023-06-06 11:24:33 -0500

    Cambios Menores

--- Test/mabeSys/src/environments/environment.ts
+++ Test/mabeSys/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //api1Url: 'http://192.168.1.38:8000/',
-  api1Url: 'http://192.168.0.29:8000/', 
+  api1Url: 'http://192.168.1.29:8000/', 
   //api1Url: 'http://192.168.1.42:8000/',
   //api1Url: 'http://192.168.1.19:8000/',
   //api1Url: 'http://192.168.1.26:8000/',
