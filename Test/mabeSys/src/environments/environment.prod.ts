3029aa347e8f514ed7f4a81dbc8693b83cd79c81
EdwinIza <eizac@est.ups.edu.ec>
2023-06-06 10:00:10 -0500

    Architect Base

--- Test/mabeSys/src/environments/environment.prod.ts
+++ Test/mabeSys/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   //api1Url: 'http://192.168.1.38:8000/',
-  //api1Url: 'http://192.168.0.29:8000/';  
-  api1Url: 'http://192.168.1.42:8000/',
+  api1Url: 'http://192.168.0.29:8000/', 
+  //api1Url: 'http://192.168.1.42:8000/',
   //api1Url: 'http://181.188.194.242:8000/';
   //api1Url: 'http://181.188.194.242:8000/';
   //api1Url: 'http://192.168.100.78:8000/';
--- Test/mabeSys/src/environments/environment.ts
+++ Test/mabeSys/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   //api1Url: 'http://192.168.1.38:8000/',
-  //api1Url: 'http://192.168.0.29:8000/';  
-  api1Url: 'http://192.168.1.42:8000/',
+  api1Url: 'http://192.168.0.29:8000/', 
+  //api1Url: 'http://192.168.1.42:8000/',
   //api1Url: 'http://192.168.1.19:8000/',
   //api1Url: 'http://181.188.194.242:8000/';
   //api1Url: 'http://181.188.194.242:8000/';
