5730bad339c6f562b216452261d91f1700d03aef
EdwinIza <eizac@est.ups.edu.ec>
2023-06-15 10:09:44 -0500

    Cambios Calendarios Responsive

--- Test/mabeSys/src/environments/environment.prod.ts
+++ Test/mabeSys/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   //api1Url: 'http://192.168.1.38:8000/',
+  //api1Url: 'http://192.168.1.29:8000/',
   api1Url: 'http://192.168.1.29:8000/', 
   //api1Url: 'http://192.168.1.42:8000/',
   //api1Url: 'http://181.188.194.242:8000/';
