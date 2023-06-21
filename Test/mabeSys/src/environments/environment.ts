248d72bdbd1fefef01c2b956811922bb06c540dc
EdwinIza <eizac@est.ups.edu.ec>
2023-06-21 10:03:32 -0500

    card tabla agregar gastos

--- Test/mabeSys/src/environments/environment.ts
+++ Test/mabeSys/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   //api1Url: 'http://192.168.1.38:8000/',
-  api1Url: 'http://192.168.1.27:8000/',
+  //api1Url: 'http://192.168.1.27:8000/',
+  api1Url: 'http://192.168.1.24:8000/',
   //api1Url: 'http://192.168.1.29:8000/', 
   //api1Url: 'http://192.168.1.42:8000/',
   //api1Url: 'http://192.168.1.19:8000/',
